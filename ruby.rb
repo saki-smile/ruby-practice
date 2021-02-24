15cc45057b12257230130aeff93c81894e1b2ebe
saki-smile <sa.su.ko0419@gmail.com>
2021-02-24 17:17:54 +0000

    add practice

--- ruby.rb
+++ ruby.rb
@@ -60,11 +60,117 @@
 # puts n
 
 
-puts 0.1 * 3.0
-puts 0.1 * 3.0 == 0.3
-puts 0.1r * 3.0r
-puts 0.1r * 3.0r == 0.3
-a = 0.1
-b  = 3.0
-puts a.rationalize * b.rationalize
-puts (0.1r * 3.0r).to_f
+# 丸め誤差
+#   puts 0.1 * 3.0
+#   puts 0.1 * 3.0 == 0.3
+#   puts 0.1r * 3.0r
+#   puts 0.1r * 3.0r == 0.3
+#   a = 0.1
+#   b  = 3.0
+#   puts a.rationalize * b.rationalize
+#   puts (0.1r * 3.0r).to_f
+
+
+# 論理演算子
+#   t1 = true
+#   t2 = true
+#   f1 = false
+#   f2 = false
+#   puts t1 && t2
+#   puts t1 && f1
+#   puts !(t1 && f1)
+#   puts t1 || f1
+#   puts f1 || f2
+#   puts t1 && t2 || f1 && f2
+#   puts t1 && (t2 || f1) && f2
+#   puts !t1
+#   puts !f1
+
+
+# if文
+# n = gets.to_i
+# n = 11
+# if n > 10
+#   puts '１０より大きい'
+# else
+#   puts '１０以下'
+# end
+
+# def greeting(country)
+#   if country == 'japan'
+#     "こんにちは"
+#   elsif country == 'us'
+#     "Hello"
+#   elsif country == 'italy'
+#     "ciao"
+#   else
+#     "???"
+#   end
+# end
+# a = 'us'
+# puts greeting(a)
+
+# point = 7
+# day = 1
+# if day == 1
+#   point *= 5
+# end
+# puts point
+
+# 後置if
+# point = 7
+# day = 1
+# point *= 5 if day == 1
+# puts point
+
+#   then
+# def greeting(country)
+#   if country == 'japan' then "こんにちは"
+#   elsif country == 'us' then "Hello"
+#   elsif country == 'italy' then  "ciao"
+#   else "???"
+#   end
+# end
+# a = 'japan'
+# puts greeting(a)
+
+
+# def add(a,b)
+#   a + b
+# end
+# puts add(1,3)
+
+# def hello_world(name)
+#   "Hello,World #{name}"
+# end
+# puts hello_world('saki')
+
+
+# def greeting(country)
+#   if country == 'japan'
+#     'こんにちは'
+#   else
+#     'hello'
+#   end
+# end
+# puts greeting('us')
+
+# def hello
+#   'Hello!!'
+# end
+# puts hello
+
+
+def fizz_buzz(num)
+  if num % 15 == 0
+    'FizzBuzz'
+  elsif num % 5 == 0
+    'Buzz'
+  elsif num % 3 == 0
+    'Fizz'
+  else
+    num.to_s
+  end
+end
+puts fizz_buzz(8)
+
